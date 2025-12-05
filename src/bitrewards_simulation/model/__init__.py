fa6c3f68eefcb11689a7742982b07fa9379ae6a5
yesthisischris <christopher.valleau@gmail.com>
2025-12-05 17:21:16 +0300

    Remove legacy simulation package and refresh story pack

--- src/bitrewards_simulation/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from bitrewards_simulation.model.agents import CreatorAgent, EconomicAgent, UserAgent
-from bitrewards_simulation.model.bitrewards_model import BitRewardsModel
-
-__all__ = [
-    "BitRewardsModel",
-    "CreatorAgent",
-    "EconomicAgent",
-    "UserAgent",
-]
